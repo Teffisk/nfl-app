6811ca58162acdff5a7434038fa23cef23996c45
Ben Atkins <benjatkins13@gmail.com>
2019-07-25 20:45:12 -0500

    ben commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Let's build some shit!</div>;
+  return <div className="App">Titans>Seahawks</div>;
 }
 
 export default App;
